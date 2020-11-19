0f034150142878e2fc5ec7a833f51b391bff7cb6
A-rgb123 <74705419+A-rgb123@users.noreply.github.com>
2020-11-19 13:58:20 +0530

    Add files via upload

---
+++ calcuuuuuuuu.py
@@ -0,0 +1,20 @@
+a = input("Hi who are you.  ")
+#First Question
+if a <= "Hacker":
+    b = input("HI Boss What's going on.  ")
+    #Second Question
+    if b <= "all good pyro what about you":
+        c = input("Nothing special my boss just initializing your files.  ")
+        #Third Question
+        if c <= "ok keep going with them.  ":
+            d = input("Any device you want to access. ")
+            #Fourth Question
+            if d <= "yes shubhams mobile":
+                e = input("As you wish here is the link https://mobile-tracker-free.com/dashboard")
+           
+
+
+
+
+
+    
